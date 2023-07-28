fc777beefc64d4ed0137b523c72451bc79e398ed
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-07-28 20:47:25 +0200

    basic operators

---
+++ basic-operators.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// + - * / %
+// < > <= >=
+// ! == && ||
+
+//let valueOne: Double = 55
+//let valueTwo: Double = 88
+//let sum = valueOne + valueTwo
+//let sum1 = valueOne / valueTwo
+
+//let valueOne = 100
+//let valueTwo = 88
+//let sum = valueOne % valueTwo
+
+var valueOne = 100
+let valueTwo = 100
+
+let sum = valueOne == valueTwo
+let sum1 = valueOne != valueTwo
+
+valueOne -= 1
