9acc41fc63c5ed515ee5cb6f886c383bb35304a1
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-07-28 19:48:53 +0200

    Function

---
+++ funcions.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//func printInstructorsName() {
+//    print("Sean Allen")
+//}
+//
+//printInstructorsName()
+
+func printInstructors(name: String) {
+   print(name)
+
+}
+
+printInstructors(name: "Sean Allen")
+
+//func add(firstNumber: Int, to secondNumer: Int) -> Int {
+func add(firstNumber: Int, secondNumer: Int) -> Int {
+    
+    let sum = firstNumber + secondNumer
+    return sum
+}
+
+//add(firstNumber: 18, to: 22)
+
+add(firstNumber: 22, secondNumer: 11)
+
