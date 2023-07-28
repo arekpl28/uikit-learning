bbaba4aaa0451f8e14e581a78d2a9e3bd9de5ce4
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-07-28 19:58:11 +0200

    if and else

---
+++ ifAndElse.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+var isDarkModeOn = false
+
+//if isDarkModeOn == true {
+//if isDarkModeOn {
+//    print("That's how it should be.")
+//} else {
+//    print("You are a psycho.")
+//}
+
+//var myHighScore = 565
+//var yourHighScore = 65
+//
+//if myHighScore > yourHighScore {
+//    print("I win")
+//} else {
+//    print("You win")
+//}
+
+var highScore = 111
+
+if highScore > 600 {
+    print("You are best")
+} else if highScore > 250{
+    print("You're average")
+} else if highScore > 100 {
+    print("You need improvment")
+} else {
+    print("Yikes")
+}
