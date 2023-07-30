1c25e928e0488465375832ea66a6a6038ea1c24f
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-07-30 14:55:09 +0200

    Generate array of random colors, logic to UITableView

--- RandomColors/RandomColors/ColorsTableVCViewController.swift
+++ RandomColors/RandomColors/ColorsTableVCViewController.swift
@@ -9,20 +9,43 @@ import UIKit
 
 class ColorsTableVCViewController: UIViewController {
     
+    var colors: [UIColor] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addRandomColors()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func addRandomColors() {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        }
+    }
+    
+    func createRandomColor() -> UIColor {
+        
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
     }
 }
 
 extension ColorsTableVCViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell") else {
+            return UITableViewCell()
+        }
+                let color = colors[indexPath.row]
+        cell.backgroundColor = color
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
