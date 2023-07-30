97d96c5206f2f87f5f8c160c0737ada59a85baff
Arkadiusz Plumbaum <arekpl28@wp.pl>
2023-07-30 18:04:05 +0200

    added comments to describe the purpose of each method

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -8,44 +8,53 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
-    @IBOutlet var cardImageView: UIImageView!
-    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var cardImageView: UIImageView! // Image view to display cards
+    @IBOutlet var buttons: [UIButton]! // Array of buttons (Stop and Restart buttons)
     
+    // Array to store the card images (Deck.allValues should be an array of UIImage)
     var cards: [UIImage] = Deck.allValues
-    var timer: Timer!
+    
+    var timer: Timer! // Timer to show random images
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        startTimer()
+        startTimer() // Start the timer to show random images
         for button in buttons {
-            button.layer.cornerRadius = 8
+            button.layer.cornerRadius = 8 // Round the corners of the buttons
         }
     }
     
+    // Function called when the view is about to disappear
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        timer.invalidate()
+        timer.invalidate() // Invalidate the timer when the view is about to disappear
     }
     
+    // Function to start the timer for displaying random images
     func startTimer() {
         timer = Timer.scheduledTimer(timeInterval: 0.1,
                                      target: self,
-                                     selector: #selector(showRandomImage),
+                                     selector: #selector(showRandomImage), // Call showRandomImage() method
                                      userInfo: nil,
-                                     repeats: true)
+                                     repeats: true) // Repeats every 0.1 seconds
     }
     
+    // Function to display a random image on the cardImageView
     @objc func showRandomImage() {
-        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+        // Get a random image from the cards array and set it as the image of the cardImageView
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS") // If cards array is empty, set the default image "AS"
     }
     
+    // Action when the "Stop" button is tapped
     @IBAction func stopButtonTapped(_ sender: UIButton) {
-        timer.invalidate()
+        timer.invalidate() // Stop the timer when the "Stop" button is tapped
     }
     
+    // Action when the "Restart" button is tapped
     @IBAction func restartButtonTapped(_ sender: UIButton) {
-        timer.invalidate()
-        startTimer()
+        timer.invalidate() // Stop the timer
+        startTimer() // Start the timer again to show random images
     }
 }
+
